9dae5740fbfa9c734c77ba2045feeb8c6016ab42
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-12-07 21:42:00 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+list = ["ab", "cd", "ef"]
+
+def my_each(array)
+  i = 0
+  while i < array.length
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
+  end
+  return array 
+end
+
+my_each(list) do |i|
 end
