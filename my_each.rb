f51c74c9bead6e257ea00a42169b12ae0d6caee1
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-11 18:23:47 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arg)
+  if block_given?
+    i = 0
+    
+  while i < arg.length
+    yield(arg[i])
+    i = i + 1
+  end
+  arg
+else
+  return "Missing Block."
+end
+end
+
+arg = [1, 2, 3, 4]
+
+my_each(arg) do |i|
+  return i
 end
