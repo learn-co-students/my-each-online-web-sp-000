a03bd40270ce0278432558e91c7ca6b7235904c3
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-10 14:31:54 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
-def my_each(collection) 
-  i = 0 
-  while i < collection.length
-  yield(collection[i])
-  i = i + 1
-  end 
+def my_each(collection)
+  counter = 0 
+  while counter < collection.length 
+  yield (collection[counter])
+  counter += 1 
+  end
   collection 
 end   
+
