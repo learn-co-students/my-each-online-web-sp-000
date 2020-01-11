9929dce589943386b37fa6d571b7b9c83b45d518
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-01-11 16:22:23 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+list = ["ab", "cd", "ef"]
+
+def my_each(array)
+  i = 0
+  while i < array.length 
+    yield(array[i])
+      i = i + 1
+      
+  end
+  return array
 end
+
+my_each(list) {|i| "#{i}"}
