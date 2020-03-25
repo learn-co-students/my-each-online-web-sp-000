d68297ab1e25bb4b56cd84be5810555d70ef1542
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-25 15:41:33 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+# Building my own method of the each method
+def my_each(array)
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    array
+  else
+    nil
+  end
 end
