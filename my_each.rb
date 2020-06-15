820b62c8cd5aa308adc8e9ae9e08449cea1e1e38
reevesalic <reevesalic@gmail.com>
2020-06-15 18:27:22 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,16 @@
+#i = ["cats", "ferrets", "fox"]
 
-def my_each(words) # put argument(s) here
+def my_each(words) #do |item|
+  # put argument(s) here
   # code here
-  
-  words = []
-  my_each(words) do |item|
-  item
+  item = 0
+  while item < words.length
+    yield(words[item])
+    item = item + 1 
     
+ end
+ 
 end
-end
+
+
+
