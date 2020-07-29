ee20b53508f15c507993fbf98e98e3cd0a853998
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-07-29 00:15:50 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+    if block_given?
+      i = array.count - 1
+      index = 0
+      while index <= i
+        yield(array[index])
+        index += 1
+      end
+    else 
+      array
+  end
 end
