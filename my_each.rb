19f65c13bcdd774ba4a01d3a610ab52b6ac4cc7c
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-22 19:41:41 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  if block_given?
+    i = 0
+    while i < arr.length
+      yield(arr[i])
+      i = i + 1
+    end
+  end
+  arr
 end
