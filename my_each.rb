d4b5016c5a417a55d44358ee0c282d14ae863f68
Jouse then <jthen93@gmail.com>
2021-02-15 23:28:02 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i = 0
+  if block_given?
+   while i < collection.length
+      yield(collection[i])
+      i = i + 1
+    end
+    
+    return collection
+  end
 end
