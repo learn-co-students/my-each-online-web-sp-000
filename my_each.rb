24886c77626c21beb54b73d89cd4a8c06eab2374
James Tillman <james@itme-music.com>
2021-05-29 22:15:58 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
+#this convaluded brain teaser is building the each do command using a while loop. WITHOUT USING |*uts EITHER.
+# while adds the complication of the variable i (in order to give the while loop a # of iterations to do)
+# yield is substituting for |*uts in this case, to help return the the original array
+#wow, it does nothing (except hurt my brain)
+
+
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+  while i < array.length
+    yield(array[i])
+    #some more code here
+    i += 1
+  end
+  array
 end
+
+
+#note that just writing a p uts in this code breaks it, even a comment
