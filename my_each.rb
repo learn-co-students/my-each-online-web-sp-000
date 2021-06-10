e218058465fcedbdd3e55dbb0243f65a53f29d57
WildBeanOnIt <julioaguilera337@gmail.com>
2021-06-09 21:04:35 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
 end
+
+  
+  # put argument(s) here
+  # code here
+
+# my_each(['hi', 'hello', 'bye', 'goodbye']) do |names|
+#   "#{names}"
+# end
